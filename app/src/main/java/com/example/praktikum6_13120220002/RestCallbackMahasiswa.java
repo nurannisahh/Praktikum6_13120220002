b8b51dd461b1e52caf62c17a42e5d98a363e6320
Naull <152953821+nurannisahh@users.noreply.github.com>
2024-05-23 20:18:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/praktikum6_13120220002/RestCallbackMahasiswa.java
@@ -0,0 +1,7 @@
+package com.example.praktikum6_13120220002;
+
+import java.util.ArrayList;
+
+public interface RestCallbackMahasiswa {
+    void requestDataMhsSuccess(ArrayList<Mahasiswa> arrayList);
+}
